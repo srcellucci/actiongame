4c24c784aaf09343578b474c6fad5afb6cac1fa8
Sara Cellucci <celluc44@students.rowan.edu>
2020-02-15 04:00:18 -0500

    Added tiles and player.
    Added tileset to tile palette and painted a rough test level, added player and started scripting move-by-recoil.

---
+++ Assets/mouseShootRecoil.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseShootRecoil : MonoBehaviour
+{
+	
+	private Vector2 toPushDirection;
+	private bool push;
+	private Transform transform;
+	public float pushStrength;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+		push = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	
+	void FixedUpdate() {
+		
+	}
+	
+	void OnMouseDown() {
+		push = true;
+		//Vector from mouse's world coordinates to player coordinates.
+		toPushDirection = position - Camera.ViewportToWorldPoint(Input.MousePosition());
+	}
+}
